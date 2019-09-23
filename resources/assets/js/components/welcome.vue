ff40c7ebde05d9944497ffe7e109fefbce8f2585
meideros <lucasgnacadja99@gmail.com>
2019-09-23 19:18:38 +0200

    initial commit

---
+++ resources/assets/js/components/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1 class="display-4">Welcome on Masonite Framewoork</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "welcome"
+    }
+</script>
+
+<style scoped>
+
+</style>
